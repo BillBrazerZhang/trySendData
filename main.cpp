44232c5db0cbd398d04cc18ad7cdcb991bc860c0
WenyuZhang1995 <wez078@ucsd.edu>
2019-11-18 19:14:00 -0800

    reformat for IMU data structure

--- main.cpp
+++ main.cpp
@@ -13,7 +13,9 @@ using namespace std;
 */
 
 
-const char *channels[] = {"C3","C4","Cz","FPz","POz","CPz","O1","O2"};
+const char *channels[] = {"imu1_Ax","imu1_Ay","imu1_Az","imu1_Gx","imu1_Gy","imu1_Gz",
+                          "imu2_Ax","imu2_Ay","imu2_Az","imu2_Gx","imu2_Gy","imu2_Gz",
+                          "imu3_Ax","imu3_Ay","imu3_Az","imu3_Gx","imu3_Gy","imu3_Gz"};
 
 int main(int argc, char* argv[]) {
     string name, type;
@@ -29,16 +31,23 @@ int main(int argc, char* argv[]) {
     try {
 
         // make a new stream_info (100 Hz)
-        lsl::stream_info info(name,type,8,1,lsl::cf_float32,string(name)+=type);
+        lsl::stream_info info(name,type,18,100,lsl::cf_float32,string(name)+=type);
 
         // add some description fields
-        info.desc().append_child_value("manufacturer", "BioSemi");
+        info.desc().append_child_value("manufacturer", "Bosch");
         lsl::xml_element chns = info.desc().append_child("channels");
-        for (int k=0;k<8;k++)
-            chns.append_child("channel")
-                    .append_child_value("label",channels[k])
-                    .append_child_value("unit","microvolts")
-                    .append_child_value("type","EEG");
+        for (int u=0; u<3; u++) {
+            for (int k = u * 6; k < u * 6 + 3; k++)
+                chns.append_child("channel")
+                        .append_child_value("label", channels[k])
+                        .append_child_value("unit", "mPs2")
+                        .append_child_value("type", "ACC");
+            for (int k = u * 6 + 3; k < u * 6 + 6; k++)
+                chns.append_child("channel")
+                        .append_child_value("label", channels[k])
+                        .append_child_value("unit", "radPs")
+                        .append_child_value("type", "GYRO");
+        }
 
         // make a new outlet
         lsl::stream_outlet outlet(info);
@@ -49,10 +58,10 @@ int main(int argc, char* argv[]) {
         for(unsigned t=0;;t++) {
 
             // wait a bit and create random data
-            while (((double)clock())/CLOCKS_PER_SEC < starttime + t*1);
-            float sample[8];
-            for (int c=0;c<8;c++){
-                sample[c] = (float)((rand()%1500)/500.0-1.5);
+            while (((double)clock())/CLOCKS_PER_SEC < starttime + t*0.01); // set the data frequency (now 100Hz)
+            float sample[18];
+            for (int c=0;c<18;c++){
+                sample[c] = (float)((rand()%1500)/500.0-1.5)*5*((c%6)+1) + 20*(c%6);
                 std::cout << sample[c] << " ";
             }
             std::cout << std::endl;
